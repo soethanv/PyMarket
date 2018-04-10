f74207195370c1113f54551a7d3d7e86b8d188b3
krakencode <daltonsumrall@gmail.com>
2018-04-10 15:59:36 -0500

    Setting up database configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # this import must be below the app creation
 # to prevent circular imports. routes uses the app variable
-from app import routes
+from app import routes, models
