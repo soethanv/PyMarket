26017cab1939cf590abee26c203a5217de7aebbe
krakencode <daltonsumrall@gmail.com>
2018-04-16 13:37:49 -0500

    Initialized the database with users and added inventory/sales pages

--- views/helper.py
+++ views/helper.py
@@ -1,5 +1,7 @@
 from views.login import bp as login_bp
 from views.dashboard import bp as dashboard_bp
 from views.logout import bp as logout_bp
+from views.sales import bp as sales_bp
+from views.inventory import bp as inventory_bp
 
-all_blueprints = (login_bp, dashboard_bp, logout_bp)
+all_blueprints = (login_bp, dashboard_bp, logout_bp, sales_bp, inventory_bp)
--- views/sales.py
+++ views/sales.py
@@ -2,12 +2,12 @@ from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
 
 
-bp = Blueprint('dashboard', __name__, template_folder='templates', static_folder='static')
+bp = Blueprint('sales', __name__, template_folder='templates', static_folder='static')
 
 
-@bp.route('/dashboard')
-def dashboard():
+@bp.route('/sales')
+def sales():
     if not current_user.is_authenticated:
         return redirect(url_for('login.login'))
 
-    return render_template('dashboard.html', title='Dashboard')
+    return render_template('sales.html', title='Sales Orders')
