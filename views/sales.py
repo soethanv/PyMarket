f346e5f0d5e609ea39ed71cd566310fc4dbef722
Carter Hunt <realife100@gmail.com>
2018-04-18 12:12:11 -0500

    Added sales branch and changes to sales page

--- views/sales.py
+++ views/sales.py
@@ -9,5 +9,15 @@ bp = Blueprint('sales', __name__, template_folder='templates', static_folder='st
 def sales():
     if not current_user.is_authenticated:
         return redirect(url_for('login.login'))
+    sales = get_sales()
+    return render_template('sales.html', title='Sales Orders', sales=sales)
 
-    return render_template('sales.html', title='Sales Orders')
+
+def get_sales():
+	sales = []
+	sales.append((1111, 'Carter',  'Food', 'NQ324'))
+	sales.append((2111, 'Gus',  'Food', 'LQ324'))
+	sales.append((3111, 'Dalton',  'Food', 'TQ324'))
+	sales.append((4111, 'Julia',  'Food', 'ZQ324'))
+	sales.append((5111, 'Soe',  'Food', 'WQ324'))
+	return sales
