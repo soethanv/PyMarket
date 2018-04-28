07dbab434b25f0a26a8fc2cc16bb82f55b4fc115
nackster <realife100@gmail.com>
2018-04-28 17:26:36 -0500

    Real orders are implemented in table

--- views/sales.py
+++ views/sales.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required
-
+from models.crud_operations import read_all_orders, get_order_items, update_order_status
+from datetime import datetime
 
 bp = Blueprint('sales', __name__, template_folder='templates', static_folder='static')
 
@@ -9,31 +10,32 @@ NumRows = 0
 @bp.route('/sales')
 @login_required
 def sales():
-    sales = get_sales()
+    sales = get_orders()
     salesLen = len(sales)
-    inventory = get_inventory()
+    inventory = get_inventory(sales)
     return render_template('sales.html', title='Sales Orders', sales=sales, inventory=inventory)
 
+def get_orders():
+	order = []
+	for sales in read_all_orders():
+		order.append((sales.poID, sales.customerID, sales.createdDt, sales.status))
 
-def get_sales():
-	sales = []
-	sales.append(('1111', 'Carter',  'Food', 'NQ324'))
-	sales.append(('2111', 'Gus',  'Food', 'LQ324'))
-	sales.append(('3111', 'Dalton',  'Food', 'TQ324'))
-	sales.append(('4111', 'Julia',  'Food', 'ZQ324'))
-	sales.append(('5111', 'Soe',  'Food', 'WQ324'))
-	return sales
+	return order
 
-def get_inventory():
+def get_inventory(salesparm):
 	inventory = []
+	
+	for po in salesparm:
+		print(get_order_items(po[0]))
+	
 	inventory.append([123, 'Cabbage', 'Vegetables', 200, 233])
 	inventory.append([421, 'Wheat', 'Grain', 1000, 3000])
-	inventory.append([344, 'Barley', 'Grain', 1000, 3000])
-	inventory.append([424, 'Apple', 'Fruit', 100, 200])
+	#inventory.append([344, 'Barley', 'Grain', 1000, 3000])
+	#inventory.append([424, 'Apple', 'Fruit', 100, 200])
 	return inventory
 
 
-#def filled():
-
+def filled():
+	update_order_status()
 
 
