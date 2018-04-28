b2d33b9c590f3c74343b5ea740c2acd5098a6d4f
Gustavo Chavez <cascade144@live.com>
2018-04-28 18:24:46 -0500

    Added delete and edit functions/minor html changes.

--- views/inventory.py
+++ views/inventory.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 from flask_login import login_required
-from models.crud_operations import read_all_products, read_product_batches
+from models.crud_operations import read_all_products, read_product_batches, read_single_product, \
+create_product, update_product_name, update_product_reorder_point, update_product_price, delete_product
 from datetime import datetime
 
 bp = Blueprint('inventory', __name__, template_folder='templates', static_folder='static')
@@ -15,22 +16,43 @@ def inventory():
 
 @bp.route('/inventoryadder', methods=['POST'])
 def handle_add():
-    print("I am adding an product")
-
+    print('I am adding an product')
     SKU = request.form['SKU']
     name = request.form['Name']
     reorder_point = request.form['RP']
-    quantity = request.form['Amount']
     price = request.form['Price']
-    category = request.form['Cat']
-    stock = request.form['Stock']
+    category = request.form['Category']
+    stock = request.form['Quantity']
     create_product(SKU, name, category, price, reorder_point, stock)
     return redirect('/inventory', code=302)
 
-@bp.route('/inventoryediter', methods=['POST'])
+@bp.route('/inventoryeditor', methods=['POST'])
 def handle_edit():
-    print("I am editing a product")
+    print('I am editing a product')
+    SKU = request.form['SKU']
+    # Compare with database
+    product = read_single_product(SKU)
+    print(product)
+    name = request.form['Name']
+    if product.name != name:
+        update_product_name(SKU, name)
+    reorder_point = request.form['RP']
+    if product.reorder_point != reorder_point:
+        update_product_reorder_point(SKU, reorder_point)
+    price = request.form['Price']
+    if product.price != price:
+        update_product_price(SKU, price)
+    category = request.form['Category']
+    stock = request.form['Quantity']
     return redirect('/inventory', code=302)
+
+@bp.route('/inventorydelete', methods=['POST'])
+def handle_delete():
+    print('I am deleting a product')
+    SKU = request.form['row_sku']
+    print(SKU)
+    # delete_product(SKU)
+    return jsonify(status='success')
 
 def get_inventory():
     inventory = []
@@ -45,13 +67,8 @@ def get_batches_with(SKU):
 
 def check_expiration_date(batch_expiration):
     now = datetime.utcnow()
-    # tokens = batch_expiration.split(' ')
-    # tokens = tokens[0].split('-')
-    # expiration = datetime.date(tokens[0], tokens[1], tokens[2])
-    print(type(batch_expiration))
-    print(type(now))
-    diff = batch_expiration - now 
+    diff = batch_expiration - now
     if diff.days <= 7:
         return True
-    else: 
+    else:
         return False
