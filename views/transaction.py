d7fde09e833e4415ffebcd067731287de8b6e09c
krakencode <daltonsumrall@gmail.com>
2018-04-28 17:12:23 -0500

    Added links to incoming/outgoing pages to the dashboard

--- views/transaction.py
+++ views/transaction.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from flask_login import login_required
 from models.crud_operations import get_all_incoming_transactions, get_all_outgoing_transactions
 
@@ -22,12 +22,3 @@ def outgoing_transactions():
     for tr in get_all_outgoing_transactions():
         transactions.append((tr.outTransactionID, tr.customerID, tr.SKU, tr.quantity, tr.from_batches, tr.transactionDate))
     return render_template('transaction.html', title='Outgoing Transactions', transactions=transactions, display='OUTGOING')
-
-
-# @bp.route('/outtransaction', methods=['GET'])
-# @login_required
-# def inventory():
-#     transactions = get_all_incoming_transactions()
-#     inventory = get_inventory()
-#     get_batches_with(90821)
-#     return render_template('intransaction.html', title='Incoming Transactions', inventory=inventory, get_batches_with=get_batches_with, check_expiration_date=check_expiration_date)
